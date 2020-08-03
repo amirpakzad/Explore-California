9bd382eb95f4d3c3e1b85c1dc5f08fe94af7c2d6
AmirPakzad <GoldenSmrv@gmail.com>
2020-08-03 13:32:58 +0430

    Add project files.

---
+++ SignalR App/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using SignalR_App.Models;
+
+namespace SignalR_App
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string name, string text)
+        {
+            var message = new ChatMessage
+            {
+                SenderName = name,
+                Text = text,
+                SentAt = DateTimeOffset.Now
+            };
+            //Broadcast to all clients
+            await Clients
+                .All
+                .SendAsync("ReceiveMessage",
+                    message.SenderName,
+                    message.SentAt,
+                    message.Text);
+        }
+    }
+}
